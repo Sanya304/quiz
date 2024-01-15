7c5f90a7996d2df3f400bc94112af0141b743142
Sanya304 <132752211+Sanya304@users.noreply.github.com>
2024-01-15 12:37:14 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,50 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the father of computer",
+    answer: "Charles Babbage",
+    options: [
+      "James Gosling",
+      "Charles Babbage",
+      "Dennis Ritchie",
+      "Bjarne Stroustrup",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What is the full form of CPU",
+    answer: "Central Processing Unit",
+    options: [
+      "Computer Processing Unit",
+
+      "Computer Principle Unit",
+      "Central Processing Unit",
+      "Control Processing Unit",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      " Which of the following computer language is written in binary codes only?",
+    answer: "Machine language",
+    options: ["pascal", "Machine language", "C", "C#"],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "Which of the following is the smallest unit of data in a computer?",
+    answer: "Bit",
+    options: ["Bit", "KB", "Nibble", "Byte"],
+  },
+];
